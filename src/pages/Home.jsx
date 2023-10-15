09a5c4b8a4620dc729cd671fbc5ed76386f9e477
Sakura Tanaka <sito6496t@gmail.com>
2023-10-15 13:29:13 +0200

    requirement filled

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,23 @@
-// TODO: Import necessary modules and data
+import { Link } from "react-router-dom";
+import { useRef } from "react";
+import songsData from "../data/songs.json";
+
 function Home() {
-  // TODO: Implement logic to display the list of songs
+  const songs = useRef(songsData.songs);
+
   return (
     <>
-      <div>
+      <div className="home-wrapper">
         <h1>Rolling Stones 500 Greatest Songs of all Time</h1>
-        {/* TODO: Display the list of songs with links */}
+        <div className="songs-wrapper">
+          {songs.current.map((obj) => (
+            <Link to={`/song/${obj.title.replaceAll(" ", "-")}`} key={obj.rank}>
+              <div className="song-box">
+                <h3>{obj.title}</h3>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </>
   );
--- src/pages/SongInfo.jsx
+++ src/pages/SongInfo.jsx
@@ -1,14 +1,29 @@
-// TODO: Import necessary modules and data
-
-// TODO: Destrcutrue props to receive information coming in from the parent comp
+import { useParams } from "react-router-dom";
+import { useMemo } from "react";
+import songsData from "../data/songs.json";
 function SongInfo() {
   // TODO: Fetch the song title from the URL and find the corresponding song data and add a conditional to display some simple html if there is no song :)
+
+  const { songsTitle } = useParams();
+
+  const searchedSong = useMemo(() => {
+    return songsData.songs.find((song) => song.tile === songsTitle);
+  }, [songsTitle]);
+
+  const { title, artist, album, year } = searchedSong;
 
   return (
     <>
       <div>
         <h2>Song Information</h2>
         {/* TODO: Display song details or a "Song not found!" message */}
+
+        <div className="song-card">
+          <h3>{title}</h3>
+          <p>{artist}</p>
+          <p>{year}</p>
+          <p>{album}</p>
+        </div>
       </div>
     </>
   );
